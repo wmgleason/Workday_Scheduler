c337cf852965a8eee622562f7ec09de53bb9242d
Wanda Gleason <oglissane@gmail.com>
2021-01-24 20:59:52 -0500

    Assigning different colors to blocks in the past, present, future for ease of use and readability.

---
+++ Assets/JS/script.js
@@ -0,0 +1,26 @@
+// opening with document ready to make sure page loads correctly
+$(document).ready(function(){
+    // trying to use dayjs to get current date - not working although it looks right to me
+    const dayjs = require("dayjs");
+    let hour = dayjs().hours();
+    console.log(hour.format());
+//day.js date for top of page
+    function getDate(){
+        $("#currentDay").text(dayjs().format('MMMM Do YYYY, h:mm:ss a'));
+    };
+
+// Adding a different color to rows depending on whether they are in the past, the present, or the future
+function colorSchedule(){
+    $("input").each(function(){
+        var rowHour = $(this).attr("id");
+        var rowNumber = parseInt(rowHour);
+        if (rowNumber === hour){
+            $(this).addClass("present");
+        } else if (rowNumber < hour){
+            $(this).addClass("past");
+        } else {
+            $(this).addClass("future");
+        };
+    });
+}
+});
