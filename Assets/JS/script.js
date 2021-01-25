7e9955ea1ccf5e0b9868c51ce8f61c7b7afb594d
Wanda Gleason <oglissane@gmail.com>
2021-01-24 22:43:40 -0500

    Added function to display user input items saved in local storage.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,16 +1,14 @@
 // opening with document ready to make sure page loads correctly
 $(document).ready(function(){
     // trying to use dayjs to get current date - not working although it looks right to me
-    const dayjs = require("dayjs");
-    let hour = dayjs().hours();
-    console.log(hour.format());
+    var hour = moment().hours();
 //day.js date for top of page
     function getDate(){
-        $("#currentDay").text(dayjs().format('MMMM Do YYYY, h:mm:ss a'));
+        $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
     };
 
 // Adding a different color to rows depending on whether they are in the past, the present, or the future
-function colorSchedule(){
+function hoursColorSchedule(){
     $("input").each(function(){
         var rowHour = $(this).attr("id");
         var rowNumber = parseInt(rowHour);
@@ -22,13 +20,22 @@ function colorSchedule(){
             $(this).addClass("future");
         };
     });
-}
+};
+//Showing the stored inputs after page refresh
+function displayStoredInputs(){
+    $(".event").each(function(){
+        var inputId = $(this).attr("id");
+        $(this).val(localStorage.getItem(inputId));
+    });
+};
 //Adding a click event which will save the user input to local storage - input will be saved despite page refresh
 $(".saveBtn").click(function(){
     var scheduleInputs = $(this).siblings(".event").val();
     var inputsLocation = $(this).siblings(".event").attr("id");
     localStorage.setItem(inputsLocation,scheduleInputs);
 });
-
-colorSchedule();
+setInterval(getDate,1000);
+hoursColorSchedule();
+setInterval(hoursColorSchedule,1000);
+displayStoredInputs();
 });
